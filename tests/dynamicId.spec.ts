acbb2e1ef5dfb6a2e792c79271974a9c8d0c3db4
Don Johnson <dj@codetestcode.io>
2023-07-06 19:38:46 -0700

    added test for dynamic id

---
+++ tests/dynamicId.spec.ts
@@ -0,0 +1,15 @@
+import {test, expect} from '@playwright/test';
+
+// Modern applications often generate dynamic IDs for elements. In this case ID is not a reliable attribute for using in element selector.
+//  By default many UI automation tools record IDs and this results in tests broken from the very beginning. An automation tool needs a way
+//  to instruct it to skip dynamic IDs when XPath is generated for an element.
+
+// Scenario
+// Record button click.
+// Then execute your test to make sure that ID is not used for button identification.
+
+
+test('dynamic id', async ({page}) => {
+await page.goto('/dynamicid');
+await page.getByRole('button', { name: 'Button with Dynamic ID' }).click();
+});

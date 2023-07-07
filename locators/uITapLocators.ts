6a4bb8e499b0223714ea0c5089cfbc9d1d7b2947
Donald Johnson <don@codetestcode.io>
2023-07-06 19:41:17 -0700

    added test for dynamic id (#1)
    Co-authored-by: Don Johnson <dj@codetestcode.io>

---
+++ locators/uITapLocators.ts
@@ -0,0 +1,33 @@
+export const UITAP_LOCS = {
+    dynamicIdPath: '/dynamicid',
+    classAttribPath: '/classattr',
+    hiddenLayerPath: '/hiddenlayer',
+    loadDelayPath: '/loaddelay',
+    ajaxDataPath: '/ajax',
+    clientDelayPath: '/clientdelay',
+    clickPath: '/click',
+    textInputPath: '/textinput',
+    scrollPath: '/scroll',
+    dynamicTablePath: '/dynamictable',
+    verifyTextPath: '/verifytext',
+    processBarPath: '/processbar',
+    visibilityPath: '/visibility',
+    sampleAppPath: '/sampleapp',
+    mouseOverPath: '/mouseover',
+    nonBreakingSpacePath: '/nbsp',
+    overlappedElementPath: '/overlapped',
+    shadowDomPath: '/shadowdom',
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
